9378b74f8f7bac6a5cf96b917bb990937f2536ae
ABHI <abhishekitte11@gmail.com>
2021-09-23 22:00:06 +0530

    [ABHISHEK] Print maximum value

---
+++ PrintArrayGenericClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    class PrintArrayGenericClass<E>
+    {
+        public E[] array;
+        public PrintArrayGenericClass(E[] array)
+        {
+            this.array = array;
+        }
+
+        public void ToPrint()
+        {
+            Console.WriteLine("Displaying array elements");
+            foreach (var element in array)
+            {
+                Console.WriteLine(element + "\t");
+            }
+        }
+    }
+}
