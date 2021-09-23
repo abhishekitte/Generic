1e3420e75d81e2b32d02fdb9fec0eecc0d15a44f
ABHI <abhishekitte11@gmail.com>
2021-09-23 21:31:03 +0530

    [ABHISHEK] Print max of 3 numbers

---
+++ PrintArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo.cs
+{
+    class PrintArray
+    {
+        public static void ToPrint(int[] intArray)
+        {
+            foreach (int element in intArray)
+            {
+                Console.WriteLine(" " + element + " ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //To take userinput
+            //Console.WriteLine("Please enter number of elements need to add into integer array");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] intArray = new int[n];
+            //for(int i=0;i<n;i++)
+            //{
+            //    intArray[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //PrintArray.ToPrint(intArray);
+
+            int[] intArray = { 20, 25, 10, 5, 4 };
+            PrintArray.ToPrint(intArray);
+            Console.ReadLine();
+        }
+    }
+}
